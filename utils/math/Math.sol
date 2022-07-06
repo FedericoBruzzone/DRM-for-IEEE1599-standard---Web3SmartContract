a44c36561e0d471535e22748847ce4edf5b511a2
Federico Bruzzone <99926684+FedericoBruzzone@users.noreply.github.com>
2022-07-06 17:07:14 +0200

    Update Math.sol

--- utils/math/Math.sol
+++ utils/math/Math.sol
@@ -12,7 +12,7 @@ library Math
     function max ( uint256 a, uint256 b ) internal pure 
                                           returns ( uint256 ) 
     {
-        return a >= b ? a : b;
+        return a > b ? a : b;
     }
 
     /**

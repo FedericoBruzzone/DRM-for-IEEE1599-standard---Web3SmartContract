5ec97bdda34b43d77c659e9b9437b71e2d02a563
Federico Bruzzone <99926684+FedericoBruzzone@users.noreply.github.com>
2022-07-06 17:16:50 +0200

    Update PlusMath.sol

--- utils/math/PlusMath.sol
+++ utils/math/PlusMath.sol
@@ -8,6 +8,44 @@ import "./SafeMath.sol";
  */
 
 library PlusMath {
+
+    /**
+     * @dev Returns the largest of two numbers.
+     */
+    function max ( uint256 a, uint256 b ) internal pure 
+                                          returns ( uint256 ) 
+    {
+        return a > b ? a : b;
+    }
+
+    /**
+     * @dev Returns the smallest of two numbers.
+     */
+    function min ( uint256 a, uint256 b ) internal pure 
+                                          returns ( uint256 ) 
+    {
+        return a < b ? a : b;
+    }
+
+    /**
+     * @dev Returns the average of two numbers. The result is rounded towards
+     * zero.
+     */
+    function average ( uint256 a, uint256 b ) internal pure 
+                                              returns ( uint256 ) 
+    {
+        return ( a & b ) + ( a ^ b ) / 2;
+    }
+
+    /**
+     * @dev Returns the ceiling of the division of two numbers.
+     */
+    function ceilDiv ( uint256 a, uint256 b ) internal pure 
+                                              returns ( uint256 ) 
+    {
+        return a / b + ( a % b == 0 ? 0 : 1 );
+    }
+    
     /**
      * @dev Returns the percentage ( b ) of a number ( a )
      */
